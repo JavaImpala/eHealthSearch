9c9e1d28071c2aee5a13aaa159d1bb9fac882b45
tor003 <tor003@NSE00101.ad.ehealthresearch.no>
2018-12-13 13:57:16 +0100

    Mapper enkelt søk

--- eHealthSearch/src/eHealthSearch/EHealthCrawler.java
+++ eHealthSearch/src/eHealthSearch/EHealthCrawler.java
@@ -1,45 +1,38 @@
 package eHealthSearch;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.URL;
-import java.net.URLConnection;
+import java.util.List;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
 
-import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
+import eHealthSearch.crawler.pubmed.Id;
+import eHealthSearch.crawler.pubmed.PubMedSearch;
 
 public class EHealthCrawler {
 	
-	public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
-
-        // Make a URL to the web page
-        URL url = new URL("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=science[journal]+AND+breast+cancer+AND+2008[pdat]");
-
-        // Get the input stream through URL Connection
-        URLConnection con = url.openConnection();
-        InputStream is =con.getInputStream();
-
-        // Once you have the Input Stream, it's just plain old Java IO stuff.
-
-        // For this case, since you are interested in getting plain-text web page
-        // I'll use a reader and output the text content to System.out.
-
-        // For binary content, it's better to directly read the bytes from stream and write
-        // to the target file.
-
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+	public static void main(String[] args)  {
+		
+		try {
+			System.setProperty("javax.xml.accessExternalDTD", "all"); 
+	        // Make a URL to the web page
+	        URL url = new URL("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pmc&term=eye");
+	        JAXBContext context = JAXBContext.newInstance(PubMedSearch.class);
+	        Unmarshaller um = context.createUnmarshaller();
+	       
+	        PubMedSearch pubSearch = (PubMedSearch) um.unmarshal(url);
+	       
+	        List<Id> list = pubSearch.getIds();
+	        
+	        System.out.println(list.size()+" resultater");
+	        
+	        for (Id id : list) {
+	            System.out.println("PMID: " + id.getId());
+	        }
         
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = factory.newDocumentBuilder();
-        Document document = builder.parse(is);
-        
-        System.out.println(document);
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+		
     }
 }
